64fbbc0120361f7118023839d95f41d1c525f40a
Jeanmarco Allain <jallain99@outlook.com>
2024-09-27 18:51:11 -0600

    todo tmrw begin work

---
+++ QuickSpawn_Addon/main.py
@@ -0,0 +1,10 @@
+bl_info = {
+    "name": "QuickSpawn",
+    "author": "OctavoPE",
+    "version": (0, 0, 1),
+    "blender": (3, 3, 0),
+    "category": "3D View",
+    "description": "Add Collections to quickly spawn them (Append/Link) when needed."
+}
+
+import bpy
